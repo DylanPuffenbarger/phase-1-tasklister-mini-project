95e25e647d9d119ac9f05629a4d762223c609920
Dylan Puffenbarger <dylanpuffenbarger@gmail.com>
2023-09-09 17:52:12 -0500

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
+class Task {
+    constructor(description){
+        this.desc = description;
+    }
+
+    toHTML = () => `
+    <li>
+        ${this.desc}
+        <button data-description="${this.desc}">✓</button>
+    </li>
+    `;
+}
+
+class TaskList {
+    constructor(...tasks){
+        this.array = [];
+        for (t in tasks){
+            this.append(t);
+        }
+    }
+    
+    addTask(description){
+        this.array.push(new Task(description.toString()))
+    }
+
+    removeTask(description){
+        this.array = this.array.filter((task) => task.desc !== description);
+    }
+
+    toHTML = () => this.array.map((task) => task.toHTML()).join('');
+}
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let taskList = new TaskList();
+    const newTaskForm = document.getElementById('create-task-form');
+    const newTaskDesc = document.getElementById('new-task-description');
+    const taskUl = document.getElementById('tasks');
+    
+    const updateList = () => {taskUl.innerHTML = taskList.toHTML();}
+    
+    document.addEventListener('submit', (e) => {
+        e.preventDefault();
+        taskList.addTask(newTaskDesc.value);
+        e.target.reset();
+        updateList();
+    });
+
+    document.addEventListener('click', (e) => {
+        if (e.target.nodeName === 'BUTTON'){
+            taskList.removeTask(e.target.dataset.description);
+            updateList();
+        }
+    })
 });
+
